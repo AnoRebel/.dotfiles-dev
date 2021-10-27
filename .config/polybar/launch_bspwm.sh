4b4c72260087c1c49b9eabda8b4070b292878a86
Ano Rebel <hacker4rebel@gmail.com>
2021-10-27 17:17:02 +0300

    Changed from HMDI to DVI

--- .config/polybar/launch_bspwm.sh
+++ .config/polybar/launch_bspwm.sh
@@ -2,16 +2,16 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Bspwm bar
-echo "---" | tee -a /tmp/polybar_hdmi.log
+echo "---" | tee -a /tmp/polybar_dp.log
 echo "---" | tee -a /tmp/polybar_vga.log
-polybar -r HDMI-1 2>&1 | tee -a /tmp/polybar_hdmi.log & disown
+polybar -r DP-1 2>&1 | tee -a /tmp/polybar_dp.log & disown
 polybar -r VGA-1 2>&1 | tee -a /tmp/polybar_vga.log & disown
 
 echo "Bspwm bars launched..."
