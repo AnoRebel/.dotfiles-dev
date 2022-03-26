b5627e20b16fe0f26a49d0f68de3e142622ad04b
Ano Rebel <hacker4rebel@gmail.com>
2022-03-26 14:07:13 +0300

    Tryna add folding stuff

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -111,5 +111,13 @@ map("n", "<A-Right>", ":vertical resize -2<CR>")
 -- Stop sourcing filetype.vim
 vim.g.did_load_filetypes = 1
 
--- Manually set Floating notifications
--- vim.notify = require("notify")
+-- local api = vim.api
+-- local folding = require("custom.folding")
+-- local current_window = api.nvim_get_current_win()
+--
+-- api.nvim_set_option("nofoldenable", true)
+-- api.nvim_set_option("foldlevel", 99)
+-- api.nvim_set_option("fillchars", "fold:\\")
+-- api.nvim_set_option('foldtext', folding.CustomFoldText())
+-- api.nvim_win_set_option(current_window, 'foldmethod', "expr")
+-- api.nvim_win_set_option(current_window, 'foldepxr', folding.GetPotionFold(api.nvim_eval("v:lnum")))
