db4eebd4fd54b7f6fab974b2bcfef61310b8dfd7
Ano Rebel <hacker4rebel@gmail.com>
2022-06-16 15:12:10 +0300

    Fixed sxhld typo. Still cant make Hypr work, sad

--- .config/hypr/polybar/launch.sh
+++ .config/hypr/polybar/launch.sh
@@ -5,11 +5,10 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch
 #polybar top &
-polybar bottom &
-polybar center &
-polybar topright &
+polybar -c ~/.config/hypr/polybar/config -r bottom &
+polybar -c ~/.config/hypr/polybar/config -r center &
+polybar -c ~/.config/hypr/polybar/config -r topright &
 
-
-echo "Bar launched..."
+echo "Bars launched..."
 
 # remember: chmod u+x ./launch.sh
