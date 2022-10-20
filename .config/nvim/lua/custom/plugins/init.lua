6338f22e1c15363b2d83b78797c9711a7cf4ac7e
Ano Rebel <hacker4rebel@gmail.com>
2022-10-20 18:20:48 +0300

    Changelog: - Changed `scrollview` to `scrollbar` - Added `hlslens`

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -488,10 +488,48 @@ return {
   --     require("virt-column").setup()
   --   end
   -- },
-  ["dstein64/nvim-scrollview"] = {
+  ["kevinhwang91/nvim-hlslens"] = {
+    config = function ()
+      require('hlslens').setup({
+        override_lens = function(render, posList, nearest, idx, relIdx)
+          local sfw = vim.v.searchforward == 1
+          local indicator, text, chunks
+          local absRelIdx = math.abs(relIdx)
+          if absRelIdx > 1 then
+            indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and '▲' or '▼')
+          elseif absRelIdx == 1 then
+            indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
+          else
+            indicator = ''
+          end
 
+          local lnum, col = unpack(posList[idx])
+          if nearest then
+            local cnt = #posList
+            if indicator ~= '' then
+              text = ('[%s %d/%d]'):format(indicator, idx, cnt)
+            else
+              text = ('[%d/%d]'):format(idx, cnt)
+            end
+            chunks = {{' ', 'Ignore'}, {text, 'HlSearchLensNear'}}
+          else
+            text = ('[%s %d]'):format(indicator, idx)
+            chunks = {{' ', 'Ignore'}, {text, 'HlSearchLens'}}
+          end
+          render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+        end
+      })
+    end,
+  },
+  ["petertriho/nvim-scrollbar"] = {
+    after = { "nvim-hlslens" },
     config = function()
-      require("scrollview").setup()
+      require("scrollbar").setup({
+        handlers = {
+          search = true, -- Requires hlslens to be loaded, will run require("scrollbar.handlers.search").setup() for you
+        },
+      })
+      require("scrollbar.handlers.search").setup()
     end
   },
   ["lambdalisue/suda.vim"] = {
